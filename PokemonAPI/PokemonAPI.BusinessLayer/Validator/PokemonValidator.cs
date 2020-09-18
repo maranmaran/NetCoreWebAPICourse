2c040b5698220527be66f8dfdebb6e89abb70509
zagarzg <zagar.zg@gmail.com>
2020-09-18 17:19:44 +0200

    [POKE-9] Create PokemonValidator

--- PokemonAPI/PokemonAPI.BusinessLayer/Validator/PokemonValidator.cs
+++ PokemonAPI/PokemonAPI.BusinessLayer/Validator/PokemonValidator.cs
@@ -10,93 +10,65 @@ using System.Text.RegularExpressions;
 
 namespace PokemonAPI.BusinessLayer.Validator
 {
-    class PokemonValidator : IPokemonValidator
+    public class PokemonValidator : IPokemonValidator
     {
-        Dictionary<string, List<string>> errorMap = new Dictionary<string, List<string>>();
-        public bool valid = true;
-      
+        private Dictionary<string, List<string>> errorMap = new Dictionary<string, List<string>>();
         
+
+        private void AddOrUpdatePokemonErrors(Dictionary<string, List<string>> errorMap, String message, String errorKey)
+        {
+            if (errorMap.ContainsKey(errorKey))
+            {
+                errorMap[errorKey].Add(message);
+            }
+
+            errorMap.Add(errorKey, new List<string>() { message });
+        }
+
         public void IsValid(PokemonDTO pokemon)
         {
 
             if (string.IsNullOrEmpty(pokemon.Name))
             {
-                List<string> errors = new List<string>();
-                errors.Add("Pokemon name is required");
-                errorMap.Add("Name",errors);
-                valid = false;
+                AddOrUpdatePokemonErrors(errorMap, "Pokemon name is required", "Name");
             }
 
             if (!(Regex.IsMatch(pokemon.Name, @"^[a-zA-Z]+$")))
             {
-                List<string> errors = new List<string>();
-                errors.Add("Pokemon name must cointain only letters");
-                errorMap.Add("Name", errors);
-                valid = false;
+                AddOrUpdatePokemonErrors(errorMap, "Pokemon name must contain only letters", "Name");
             }
 
             if (string.IsNullOrEmpty(pokemon.Avatar))
             {
-                List<string> errors = new List<string>();
-                errors.Add("Pokemon avatar is required");
-                errorMap.Add("Avatar", errors);
-                valid = false;
+                AddOrUpdatePokemonErrors(errorMap, "Pokemon avatar is required", "Avatar");
             }
 
             if (!(Regex.IsMatch(pokemon.Avatar, @"^[a-zA-Z]+$")))
             {
-                List<string> errors = new List<string>();
-                errors.Add("Pokemon avatar must cointain only letters");
-                errorMap.Add("Avatar", errors);
-                valid = false;
+                AddOrUpdatePokemonErrors(errorMap, "Pokemon avatar must contain only letters", "Avatar");
             }
 
             if (pokemon.Generation == null)
             {
-                List<string> errors = new List<string>();
-                errors.Add("Pokemon generation is required");
-                errorMap.Add("Generation", errors);
-                valid = false;
+                AddOrUpdatePokemonErrors(errorMap, "Pokemon generation is required", "Generation");
             }
 
             if (pokemon.Generation < 1 || pokemon.Generation > 8)
             {
-                List<string> errors = new List<string>();
-                errors.Add("Pokemon generation needs to be between 1 and 8 inclusive");
-                errorMap.Add("Generation", errors);
-                valid = false;
+                AddOrUpdatePokemonErrors(errorMap, "Pokemon generation must be between 1 and 8 inclusive", "Generation");
             }
             
-
             if (pokemon.Height == 0)
             {
-                List<string> errors = new List<string>();
-                errors.Add("Pokemon height is required");
-                errorMap.Add("Height", errors);
-                valid = false;
+                AddOrUpdatePokemonErrors(errorMap, "Pokemon height is required", "Height");
             }
 
             if (pokemon.Weight == 0)
             {
-                List<string> errors = new List<string>();
-                errors.Add("Pokemon weight is required");
-                errorMap.Add("Weight", errors);
-                valid = false;
+                AddOrUpdatePokemonErrors(errorMap, "Pokemon weight is required", "Weight");
             }
 
-            if (!(pokemon.Type.Equals("Normal") || pokemon.Type.Equals("Fire") || pokemon.Type.Equals("Fighting")  ||  pokemon.Type.Equals("Water") ||
-                pokemon.Type.Equals("Flying") || pokemon.Type.Equals("Grass") || pokemon.Type.Equals("Poison") || pokemon.Type.Equals("Electric") ||
-                pokemon.Type.Equals("Ground")  || pokemon.Type.Equals("Psychic") || pokemon.Type.Equals("Rock")  ||  pokemon.Type.Equals("Ice") ||
-                pokemon.Type.Equals("Bug") || pokemon.Type.Equals("Dragon") || pokemon.Type.Equals("Ghost") || pokemon.Type.Equals("Dark") ||
-                pokemon.Type.Equals("Steel") ||  pokemon.Type.Equals("Fairy")))
-            {
-                List<string> errors = new List<string>();
-                errors.Add("Pokemon type needs to be one of these types: Normal, Fire, Fighting, Water, Flying, Grass, Poison, ElectricGround, Psychic, Rock, Ice, Bug, Dragon, Ghost, Dark, Steel, Fairy");
-                errorMap.Add("Type", errors);
-                valid = false;
-            }
-
-            if (!valid)
+            if (errorMap.Count > 0)
             {
                 throw new ValidationException("pokemon", errorMap);
             }
