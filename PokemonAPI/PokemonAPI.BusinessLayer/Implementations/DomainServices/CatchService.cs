850bc8894d2825917aeff4d53a521b6eb5fa6d32
zagarzg <zagar.zg@gmail.com>
2020-09-25 13:42:41 +0200

    [POKE-13] Introduce logging

--- PokemonAPI/PokemonAPI.BusinessLayer/Implementations/DomainServices/CatchService.cs
+++ PokemonAPI/PokemonAPI.BusinessLayer/Implementations/DomainServices/CatchService.cs
@@ -1,11 +1,9 @@
 ﻿using AutoMapper;
-using Microsoft.Extensions.Logging;
 using PokemonAPI.BusinessLayer.Exceptions;
 using PokemonAPI.BusinessLayer.Interfaces;
 using PokemonAPI.DomainLayer.Entities;
 using PokemonAPI.PersistenceLayer.Interfaces;
 using System;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -17,15 +15,13 @@ namespace PokemonAPI.BusinessLayer.Implementations.DomainServices
         private readonly IMapper _mapper;
         private readonly IRepository<Pokemon> _pokeRepository;
         private readonly IChanceGenerator _chanceGenerator;
-        private readonly ILogger<CatchService> _logger;
 
-        public CatchService(IPokemonService pokemonService, IMapper mapper, IRepository<Pokemon> pokeRepository, IChanceGenerator chanceGenerator, ILogger<CatchService> logger)
+        public CatchService(IPokemonService pokemonService, IMapper mapper, IRepository<Pokemon> pokeRepository, IChanceGenerator chanceGenerator)
         {
             _pokemonService = pokemonService;
             _mapper = mapper;
             _pokeRepository = pokeRepository;
             _chanceGenerator = chanceGenerator;
-            _logger = logger;
         }
 
         public async Task CatchPokemon(Guid pokemonId, Guid trainerId, CancellationToken cancellationToken = default)
@@ -36,12 +32,7 @@ namespace PokemonAPI.BusinessLayer.Implementations.DomainServices
             if (pokemonEntity.Trainer != null)
                 throw new CatchPokemonException("Pokemon already has a trainer");
 
-            var chance = _chanceGenerator.getChance(2);
-
-            _logger.LogInformation("Chance to catch a pokemon is 50%");
-            _logger.LogDebug($"User rolled {chance} (Only rolling 0 equals to success)");
-
-            if (chance != 0)
+            if (_chanceGenerator.getChance(2) != 0)
             {
                 throw new CatchPokemonException("Unfortunately pokemon dodged your pokeball");
             }
