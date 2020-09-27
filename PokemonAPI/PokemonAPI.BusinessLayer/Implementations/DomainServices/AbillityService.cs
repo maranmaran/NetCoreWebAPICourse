13aeca7cb4c672f32136db2b80fc738a6755cc7d
zagarzg <zagar.zg@gmail.com>
2020-09-27 16:42:37 +0200

    [POKE-8] As a user I wish to retrieve list of all known pokemon abilities. so I can compare them

---
+++ PokemonAPI/PokemonAPI.BusinessLayer/Implementations/DomainServices/AbillityService.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PokemonAPI.BusinessLayer.Interfaces;
+using PokemonAPI.DomainLayer.Entities;
+using PokemonAPI.PersistenceLayer.DTOModels;
+using PokemonAPI.PersistenceLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PokemonAPI.BusinessLayer.Implementations.DomainServices
+{
+    internal class AbillityService : IAbillityService
+    {
+        private readonly IRepository<PokemonAbility> _repository;
+        private readonly IMapper _mapper;
+
+        public AbillityService(IRepository<PokemonAbility> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<AbilityDTO>> GetAll(CancellationToken cancellationToken = default)
+        {
+            var abilities = await _repository.GetAll(
+                include: source => source
+                    .Include(x => x.Ability),
+                    cancellationToken: cancellationToken);
+
+            return _mapper.Map<IEnumerable<AbilityDTO>>(abilities);
+        }
+    }
+}
---
+++ PokemonAPI/PokemonAPI.BusinessLayer/Interfaces/IAbillityService.cs
@@ -0,0 +1,14 @@
+﻿using PokemonAPI.PersistenceLayer.DTOModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PokemonAPI.BusinessLayer.Interfaces
+{
+    public interface IAbillityService
+    {
+        Task<IEnumerable<AbilityDTO>> GetAll(CancellationToken cancellationToken = default);
+    }
+}
